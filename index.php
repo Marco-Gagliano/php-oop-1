d9cba842a732eb9c7c00121c8e2e64f6146d804f
Marco-Gagliano <brazil_94@hotmail.it>
2022-06-18 17:35:21 +0200

    preparazione-stringhe

--- index.php
+++ index.php
@@ -1,6 +1,46 @@
 <?php 
 
-require_once __DIR__ . '/movies.php'
+require_once __DIR__ . '/movies.php';
+
+$aotParteUno = new Movie(
+  'L\'attacco dei giganti - Il film - Parte 1: L\'arco e la freccia cremisi',
+  '2015-05-12',
+  'Tetsuro Araki',
+  'Animazione',
+  '120',
+  '7,5/10',
+  'Per oltre cento anni le alte mura che circondano Shiganshina hanno difeso la cittadina da un pericolo che gli abitanti si rifiutano persino di nominare. Chi desidera esplorare il mondo esterno è visto come un pazzo e guardato con disprezzo. Il giovane Eren si sente però come un animale in cattività e, sebbene avvenga spesso che le squadre inviate ritornino decimate, sogna di unirsi al Corpo di Ricerca per scoprire la realtà che lo circonda. Un giorno Eren sogna l\'attacco di esseri giganteschi e, anche se al risveglio ha rimosso ogni ricordo di quanto ha visto, gli resta addosso una stranissima sensazione. Poco più tardi accade l\'imprevisto: un immenso Titano apre una breccia nelle mura di protezione. Per Eren sarà uno shock senza precedenti...',
+);
+
+$aotParteDue = new Movie(
+  'L\'attacco dei giganti. Il film. Parte 2: Le ali della libertà',
+  '2015-09-01',
+  'Testuro Araki',
+  'Animazione',
+  '114',
+  '7,8/10',
+  'Eren scopre che per tutto il tempo è stato rinchiuso nei sotterranei della sede della commissione e che verrà sottoposto al giudizio della corte marziale. La sua unica speranza di salvezza è costituita dal Corpo di Ricerca (nel quale, fra l\'altro, Eren desidera entrare da tempo). Il Corpo lo utilizzerebbe infatti in battaglia per eliminare il maggior numero possibile di nemici...',
+);
+
+$aotParteTre = new Movie(
+  'L\'attacco dei giganti: L\'urlo del risveglio',
+  '2018-01-13',
+  'Masashi Koizuka',
+  'Animazione',
+  '115',
+  '8,4/10',
+  'Per un intero secolo le mura permisero agli umani di vivere in pace. Dimenticarono il terrore di essere soggiogati e l’umiliazione di essere rinchiusi in gabbia. Tuttavia, quando i Giganti penetrarono quelle stesse mura, la battaglia per la sopravvivenza dell’umanità ebbe inizio. In seguito, mentre lo scontro continuava a mietere vittime sia tra i civili che tra i militari, si scoprì che alcuni Giganti si nascondevano tra gli umani. A cinque anni dalla prima breccia nelle mura, gli umani riuscirono a catturare uno di loro, un titano mutaforma, il Gigante Femmina Annie Leonhart.',
+);
+
+$aotParteQuattro = new Movie(
+  'Shingeki no Kyojin: Chronicle',
+  'TBA',
+  'Testuro Araki , Masashi Koizuka',
+  'Animazione',
+  'TBA',
+  'TBA',
+  'Shingeki no Kyojin: Chronicle è il quarto film riassuntivo della serie, che racconta le prime tre stagioni della serie Attack on Titan basata sul manga di Hajime Isayama.',
+);
 
 ?>
 
