75b7c7ab5686ede114d8ed4e617b7b098d95430e
Marco-Gagliano <brazil_94@hotmail.it>
2022-06-18 17:34:39 +0200

    fix-variabili

--- movies.php
+++ movies.php
@@ -2,26 +2,26 @@
 
   class Movie {
     public $titolo;
-    public $data_uscita;
+    public $data_uscita_italia;
     public $regia;
     public $genere;
-    public $durata;
+    public $durata_minuti;
     public $voto;
     public $descrizione_film;
   
 
     public function __construct($_titolo,
-                                $_data_uscita,
+                                $_data_uscita_italia,
                                 $_regia,
                                 $_genere,
-                                $_durata,
+                                $_durata_minuti,
                                 $_voto_metacritic,
                                 $_descrizione_film){
       $this->titolo = $_titolo;
-      $this->data_uscita = $_data_uscita;
+      $this->data_uscita_italia = $_data_uscita_italia;
       $this->regia = $_regia;
       $this->genere = $_genere;
-      $this->durata = $_durata;
+      $this->durata_minuti = $_durata_minuti;
       $this->voto_metacritic = $_voto_metacritic;
       $this->descrizione_film = $_descrizione_film;
   }
