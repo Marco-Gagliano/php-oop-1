aaabf112df93563ca887643f3e98dd44cabaf976
Marco-Gagliano <brazil_94@hotmail.it>
2022-06-18 16:49:41 +0200

    creazione-classe/variabili/construct/metodi

--- movies.php
+++ movies.php
@@ -1,3 +1,57 @@
 <?php 
 
+  class Movie {
+    public $titolo;
+    public $data_uscita;
+    public $regia;
+    public $genere;
+    public $durata;
+    public $voto;
+    public $descrizione_film;
+  
+
+    public function __construct($_titolo,
+                                $_data_uscita,
+                                $_regia,
+                                $_genere,
+                                $_durata,
+                                $_voto_metacritic,
+                                $_descrizione_film){
+      $this->titolo = $_titolo;
+      $this->data_uscita = $_data_uscita;
+      $this->regia = $_regia;
+      $this->genere = $_genere;
+      $this->durata = $_durata;
+      $this->voto_metacritic = $_voto_metacritic;
+      $this->descrizione_film = $_descrizione_film;
+  }
+
+    public function getTitolo(){
+      return $this->titolo;
+    }
+
+    public function getDataUscita(){
+      return $this->data_uscita;
+    }
+
+    public function getRegia(){
+      return $this->regia;
+    }
+
+    public function getGenere(){
+      return $this->genere;
+    }
+
+    public function getDurata(){
+      return $this->durata;
+    }
+
+    public function getVotoMetacritic(){
+      return $this->voto_metacritic;
+    }
+
+    public function getDescrizioneFilm(){
+      return $this->descrizione_film;
+    }
+}
 ?>
